48801810c1a54dc0b9d1c51e4e89e53335e4ebc1
digvijay-infimatrix-1 <djadhav@infimatrix.com>
2025-01-19 00:13:32 +0530

    Initial commit for CartifyX project

---
+++ src/main/java/com/digvijay/cartifyx/CartifyxApplication.java
@@ -0,0 +1,13 @@
+package com.digvijay.cartifyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartifyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartifyxApplication.class, args);
+	}
+
+}
